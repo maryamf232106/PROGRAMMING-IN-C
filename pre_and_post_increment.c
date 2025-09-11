14284c9f5e14ca537d65b21a7cadf2879bf7f64a
maryamf232106 <maryamf232106@gmail.com>
2025-09-11 12:18:47 +0530

    Add files via upload

---
+++ pre_and_post_increment.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main(){
+    int x = 13;
+    printf("The initial value of x is :%d\n",x);
+    printf("\nUsing pre increment (++x) value is printed as:%d\n",++x);
+    x=13;
+    printf("\nUsing post increment (x++) value is printed as:%d\n",x++);
+    return 0;
+
+}
